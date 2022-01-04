79b99c4a52e48f810e863bdb17f515944be3a453
Ezeki Zibzibadze <ez@jok.io>
2022-01-04 23:25:36 +0400

    fix: deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Ezeki = await ethers.getContractFactory("Ezeki");
+  const greeter = await Ezeki.deploy();
 
   await greeter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Ezeki contract deployed to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
